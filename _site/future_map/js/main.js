aeaa28eaf087c4358a154e2f445436461f77d95c
Adam Kenneweg <akennewe@uwaterloo.ca>
2019-12-12 15:27:43 -0500

    ok

--- _site/future_map/js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $("footer .social-icons a").tooltip();
-
-  $(window).scroll(function () {
-    $(".splash").css("background-position", "right " + ($(this).scrollTop() / 2) + "px");
-  });
-});
